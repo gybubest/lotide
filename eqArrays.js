c318a27b2a01fc5d587b4491b3664802af380e0c
Yu Gao <gaoyububest@gmail.com>
2020-10-31 02:47:36 +0000

    equalArray recursive comparison

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } 
-  }
-  return true;
-};
-
+// TEST/ASSERTION FUNCTIONS
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -19,7 +7,30 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    let value1 = array1[i];
+    let value2 = array2[i];
+    if (Array.isArray(value1) || Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+    } else if (value1 !== value2) {
+      return false;
+    } 
+  }
+  return true;
+};
+
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([], []), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertEqual(eqArrays([[], [4]], [[], [4]]), true);
