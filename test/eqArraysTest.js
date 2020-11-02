2bae4cf0293eabfc86bdaea9b5445756a9ce1e2b
Yu Gao <gaoyububest@gmail.com>
2020-11-02 20:21:02 +0000

    modified with Mocha & Chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,11 +1,34 @@
-const assertEqual = require('../assertEqual');
-
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-assertEqual(eqArrays([[], [4]], [[], [4]]), true);
+describe("eqArrays", () => {
+
+  it('returns true for [], []', () => {
+    assert.deepEqual(eqArrays([], []), true);
+  });
+
+  it('returns true for [1, 2, 3], [1, 2, 3]', () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it('returns false for ["1", "2", "3"], ["1", "2", 3]', () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+
+  it('returns true for [[2, 3], [4]], [[2, 3], [4]]', () => {
+    assert.deepEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+
+  it('returns false for [[2, 3], [4]], [[2, 3], [4, 5]]', () => {
+    assert.deepEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+
+  it('returns false for [[2, 3], [4]], [[2, 3], 4]', () => {
+    assert.deepEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
+
+  it('returns true for [[], [4]], [[], [4]]', () => {
+    assert.deepEqual(eqArrays([[], [4]], [[], [4]]), true);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,18 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("tail", () => {
 
-// Test Case 2: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  it('returns 2 for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual((tail(["Hello", "Lighthouse", "Labs"]).length), 2);
+  });
+
+  it('returns "Lighthouse" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual((tail(["Hello", "Lighthouse", "Labs"])[0]), "Lighthouse");
+  });
+
+  it('returns "Labs" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual((tail(["Hello", "Lighthouse", "Labs"])[1]), "Labs");
+  });
+
+});
