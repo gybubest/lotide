7d4e1928545a132b94d11a392a64b2f7705cabbe
Yu Gao <gaoyububest@gmail.com>
2020-11-02 19:20:22 +0000

    module.exports and require added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } 
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   const inspect = require('util').inspect; 
@@ -20,9 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([], []); // => should PASS
-assertArraysEqual([], [1, 2, 3]); // => should FAIL
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
+module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } 
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    // console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-    console.log(`✅✅✅ Assertion Passed.`);
-  } else {
-    // console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-    console.log(`🛑🛑🛑 Assertion Failed.`);
-  }
-};
-
-// FUNCTION IMPLEMENTATION
 const middle = function(array) {
   if (array.length <= 2) {
     return array;
@@ -40,12 +16,4 @@ const middle = function(array) {
   return result;
 };
 
-
-// TEST CODE
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), [1]);
-assertArraysEqual(middle([1, 2]), [1, 2]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
