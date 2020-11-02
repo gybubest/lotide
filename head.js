850d19668151ab42c0118417c67974a188d7d09d
Yu Gao <gaoyububest@gmail.com>
2020-11-02 18:54:49 +0000

    module.exports and require added

--- head.js
+++ head.js
@@ -1,18 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   let result = array[0];
   return result;
 };
 
-// // TEST CODE
-assertEqual(head([]), undefined)
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
